4dd632f0197ca584892163a86151ce161cbccc26
Sasanm88 <sasanm@usf.edu>
2023-03-29 16:36:30 -0400

    exact MILP

--- MIP_SPLIT.jl
+++ MIP_SPLIT.jl
@@ -63,3 +63,74 @@ function SPLIT_MIP(TT::Matrix{Float64}, demands::Vector{Int}, K::Int, W::Int, S:
     end
     return objective_value(model), tours
 end
+
+
+function MTSP_Exact(TT::Matrix{Float64}, m::Int)
+    
+    model = Model(Gurobi.Optimizer)
+    N = size(TT)[1] 
+    p = N-2
+    big_M = sum(TT) 
+    @variable(model, x[1:N, 1:N] >= 0, Bin)
+    @variable(model, c[1:N] >= 0)
+    @variable(model, u[1:N] >= 0, Int)
+    @variable(model, C_max >=0)
+
+    @objective(model, Min, C_max)
+    @constraint(model, x[1,N] ==0)
+    @constraint(model, x[N,1] ==0)
+    @constraint(model, c[1] ==0)
+    
+
+#     for i=1:N
+#         @constraint(model, C_max >= c[i])
+#     end
+
+    @constraint(model, sum(x[1,i] for i=2:N-1) == m)
+    @constraint(model, sum(x[i,N] for i=2:N-1) == m)
+    
+    for i=2:N-1
+        @constraint(model, sum(x[i,j] for j=2:N) == 1)
+        @constraint(model, sum(x[j,i] for j=1:N-1) == 1)
+    end
+    for i=1:N
+        @constraint(model, x[i,i] ==0)
+        for j=1:N
+            @constraint(model, c[j] >= c[i] + TT[i,j]*x[i,j] - big_M*(1-x[i,j]))
+        end
+    end
+    for i=1:N
+        @constraint(model, C_max >= c[i])
+    end
+         
+
+    for i=1:N
+        for j=1:N
+            if i != j
+                @constraint(model, u[i] - u[j] + p* x[i,j] <= p-1)
+            end
+        end
+    end
+
+    run_time_limit = 1000 #sec
+    set_time_limit_sec(model, run_time_limit)
+    optimize!(model)
+    
+#     tours = Vector{Vector{Int}}()
+#     i = 1
+#     X = value.(x)
+#     while (i < n)
+#         tour = Int[]
+#         for j=i:n
+#             if X[i,j] > 0
+#                 for t=i:j
+#                     push!(tour, t)
+#                 end
+#                 i = j+1
+#                 break
+#             end
+#         end
+#         push!(tours, tour)
+#     end
+    return objective_value(model), value.(x), value.(c)
+end
